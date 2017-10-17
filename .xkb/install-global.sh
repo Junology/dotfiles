b5a97ab2be4fc157dbf6ee0f3782b39431e795bf
Junology <migi.hineri.jun@gmail.com>
2017-10-17 21:23:55 +0900

    rewrite .xkb/README

--- .xkb/install-global.sh
+++ .xkb/install-global.sh
@@ -4,7 +4,6 @@ XKBPATH=/usr/share/X11/xkb
 SYMB=symbols/
 EVDEV=rules/evdev
 
-make patch
 cp -r $SYMB/* "$XKBPATH/$SYMB"
 patch -u -N -b -z .bak "$XKBPATH/$EVDEV" < ./evdev.patch
 patch -u -N -b -z .bak "$XKBPATH/$EVDEV.lst" < ./evdev.lst.patch
