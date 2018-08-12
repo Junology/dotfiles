a947f38e2a45f645bc3d4f37a0090051a187f8ff
Junology <jtachan@hotmail.com>
2018-08-12 22:07:36 +0900

    change the computation of the window size

--- .emacs.d/user_window.el
+++ .emacs.d/user_window.el
@@ -9,15 +9,27 @@
                       '("Takaoゴシック" . "unicode-bmp"))))
 
 ;; Compute a better frame size from the display resolution
+(defvar workarea-list
+  (assq 'workarea (car (display-monitor-attributes-list))))
+(unless workarea-list
+  (lambda ()
+	(setq workarea-list
+		  (0 0 (x-display-pixel-width) (x-display-pixel-height)))))
+
+(defconst workarea-left (nth 1 workarea-list))
+(defconst workarea-top (nth 2 workarea-list))
+(defconst workarea-width (nth 3 workarea-list))
+(defconst workarea-height (nth 4 workarea-list))
+
 (defun set-frame-size-according-to-resolution ()
   (interactive)
   (when (display-graphic-p)
 	(progn
       (add-to-list 'default-frame-alist
-                   (cons 'width (max 80 (/ (/ (x-display-pixel-width) 1.5)
+                   (cons 'width (max 80 (/ (/ workarea-width 1.5)
                                            (frame-char-width)))))
 	  (add-to-list 'default-frame-alist 
-				   (cons 'height (/ (- (x-display-pixel-height) 20)
+				   (cons 'height (/ (- workarea-height 20)
 									(frame-char-height)))))))
 
 (set-frame-size-according-to-resolution)
