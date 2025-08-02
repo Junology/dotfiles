bf81bad5832a55d5b32a803dc8f029dad4396428
Junology <jtachan@hotmail.com>
2025-08-02 11:35:53 +0900

    Revert "Revert "Update a lot of things""
    This reverts commit 5b29b3972cf823e075305f8b467665e843917ec1.

---
+++ .emacs.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-file (expand-file-name "custom.el" user-emacs-directory))
+ '(package-selected-packages
+   '(gnu-elpa-keyring-update atomic-chrome maxima company-coq proof-general markdown-mode markdown-preview-mode company-auctex company-c-headers company-maxima company-shell flycheck-irony company-bibtex company-irony-c-headers company-reftex flycheck-haskell haskell-mode intero yaml-mode langtool haskell-snippets yasnippet lsp-haskell lsp-mode lsp-ui company-irony irony idris-mode lua-mode flycheck company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(line-number ((t (:inherit (shadow default) :background "midnight blue" :foreground "white"))))
+ '(line-number-current-line ((t (:inherit line-number :background "firebrick" :foreground "white" :weight bold)))))
--- .emacs.d/user_tex.el
+++ .emacs.d/user_tex.el
@@ -11,10 +11,10 @@
 ;(setq kinsoku-limit 10)
 (setq LaTeX-indent-level 0)
 (setq LaTeX-item-indent 2)
-(setq TeX-output-view-style '(("^dvi$" "." "xdvi-ja '%d'")))
+;(setq TeX-output-view-style '(("^dvi$" "." "xdvi-ja '%d'")))
 (setq preview-image-type 'dvipng)
-(add-hook 'LaTeX-mode-hook 'prettify-symbols-mode)
-(add-hook 'LaTeX-mode-hook (function (lambda ()
+; (add-hook 'LaTeX-mode-hook 'prettify-symbols-mode)
+(defun my-LaTeX-mode-hook ()
   (add-to-list 'TeX-command-list
     '("pTeX" "ptex %`%S%(PDFout)%(mode)%' %t"
      TeX-run-TeX nil (plain-tex-mode) :help "Run ASCII pTeX"))
@@ -22,20 +22,76 @@
     '("pLaTeX" "platex %`%S%(PDFout)%(mode)%' %t"
      TeX-run-TeX nil (latex-mode) :help "Run ASCII pLaTeX"))
   (add-to-list 'TeX-command-list
-    '("xpdf" "acroread '%s.pdf' " TeX-run-command t nil))
-  (add-to-list 'TeX-command-list
     '("pdf" "dvipdfmx -V 4 '%s' " TeX-run-command t nil))
-  (add-to-list 'TeX-command-list
-    '("View" "xdvi-ja '%d'" TeX-run-command t nil)))))
-(add-hook 'LaTeX-mode-hook '(lambda ()
-  (setq TeX-PDF-mode t)
+;  (add-to-list 'TeX-command-list
+;    '("View" "xdvi-ja '%d'" TeX-run-command t nil))
+  (add-to-list 'TeX-view-program-selection '(output-pdf "Zathura"))
+  (add-to-list
+   'TeX-command-list 
+   '("Nomenclature" "makeindex %s.nlo -s nomencl.ist -o %s.nls"
+	 (lambda (name command file)
+	   (TeX-run-compile name command file)
+	   (TeX-process-set-variable file 'TeX-command-next TeX-command-default))
+	 nil t :help "Create nomenclature file"))
+  (TeX-source-correlate-mode 1) ; activate forward/reverse search on PDF
+  (TeX-PDF-mode 1)
   (TeX-fold-mode 1)
-  (setq prettify-symbols-unprettify-at-point t)
-  (outline-minor-mode t)))
-;; extra outline headers 
-(setq TeX-outline-extra
-    '(("^\\\\begin{thebibliography}" 2)
-      ("^\\\\end{thebibliography}" 2)))
+  ;; Pretty printer for LaTeX symbols
+  (setq prettify-symbols-unprettify-at-point 'right-edge)
+  (setq prettify-symbols-alist
+		(append '(("\\par" . ?⏎)
+				  ("\\pm" . ?±)
+				  ("\\coloneqq" . ?≔)
+				  ("\\coloneq" . (?: (cr cl -20 -8) ?-))
+				  ("\\bigoplus" . ?⨁)
+				  ("\\bigotimes" . ?⨂)
+				  ("\\dots" . ?⋯)
+				  ("\\varnothing" . ?∅))
+				prettify-symbols-alist))
+  (prettify-symbols-mode 1)
+  ;; Disable auto-indent in hitting Return
+  (electric-indent-mode -1)
+  ;; extra outline headers 
+  (outline-minor-mode t)
+  (setq TeX-outline-extra
+		'(("^\\\\begin{thebibliography}" 2)
+		  ("^\\\\end{thebibliography}" 2)))
+  ;; RefTex
+  (turn-on-reftex)
+  (add-to-list 'company-backends '(company-reftex-labels company-reftex-citations))
+  ;; flyspell setting
+  (flyspell-mode 1)
+  (defun my-add-flyspell-tex-command-regexp (e)
+	"Append E to the head of flyspell-tex-command-regexp."
+	(setq flyspell-tex-command-regexp
+	  (concat
+	   (substring flyspell-tex-command-regexp 0 2)
+	   e
+	   "\\|"
+	   (substring flyspell-tex-command-regexp 2))))
+  (with-eval-after-load "ispell"
+	(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+  ;; cleveref package
+  (TeX-add-style-hook "cleveref"
+    (lambda ()
+	  (if (boundp 'reftex-ref-style-alist)
+       (add-to-list
+        'reftex-ref-style-alist
+        '("Cleveref" "cleveref"
+          (("\\cref" ?c) ("\\Cref" ?C) ("\\cpageref" ?d) ("\\Cpageref" ?D)))))
+	  (reftex-ref-style-activate "Cleveref")
+	  (add-to-list 'reftex-ref-style-default-list "Cleveref")
+	  (TeX-add-symbols
+	   '("cref" TeX-arg-ref)
+	   '("Cref" TeX-arg-ref)
+	   '("cpageref" TeX-arg-ref)
+	   '("Cpageref" TeX-arg-ref))
+	  (with-eval-after-load "flyspell"
+		(my-add-flyspell-tex-command-regexp "[cC]\\(page\\)?ref[ 	]*{"))
+	  ))
+  (TeX-run-style-hooks "cleveref")
+  )
+(add-hook 'LaTeX-mode-hook 'my-LaTeX-mode-hook)
 
 ;; shell-escape enabled using file local variable
 (with-eval-after-load 'tex
@@ -43,7 +99,7 @@
                '(TeX-command-extra-options . "-shell-escape")))
 
 ;; Turn on RefTeX in AUCTeX
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+; (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 ;; Activate nice interface between RefTeX and AUCTeX
 (setq reftex-plug-into-AUCTeX t)
 ;; found at StackOverflow https://stackoverflow.com/questions/9682592/setting-up-reftex-tab-completion-in-emacs
