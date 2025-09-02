be81c6eb37c67bea5a66e543b9a457da220c8e67
Junology <jtachan@hotmail.com>
2025-09-02 17:52:53 +0900

    remove obsolete configs

--- dotLink.sh
+++ dotLink.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 ln -f -s ./dotfiles/.emacs.d $HOME/.emacs.d
-ln -f -s ./dotfiles/.vim $HOME/.vim
 ln -f -s ./dotfiles/.vimrc $HOME/.vimrc
 ln -f -s ./dotfiles/.gvimrc $HOME/.gvimrc
 ln -f -s ./dotfiles/.xkb $HOME/.xkb
-ln -f -s ./dotfiles/.keysnail.js $HOME/.keysnail.js
 
